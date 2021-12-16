a84bad3b6fc9abffdd374d0ba6df7a09273e33bc
Малинин Иван Викторович <ivm@mts.ru>
2021-12-16 11:57:25 +0300

    ** Правки по fixme и todo

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def main() -> None:
     """
     Генерирует список из 100 случайно сгенерированных книг и записывает в файл в формате JSON.
     """
-    books = iter(book())  # todo always iterator
+    books = book_gen()
     out = [next(books) for _ in range(100)]
     with open('out.json', 'w') as fd:
         fd.write(dumps(out, indent=2, ensure_ascii=False))
     return
 
 
-def title() -> str:
+def titles_gen() -> str:
     """
     Генератор. Возвращает случайно выбранное название из списка в файле.
     :return: str
@@ -31,22 +31,22 @@ def title() -> str:
     with open('fakebooks.txt') as fd:
         booknames = fd.readlines()
     while True:
-        yield choice(booknames).rstrip('\n').rstrip('\r')  # fixme rstrip()
+        yield choice(booknames).rstrip()
 
 def year() -> int:
     """
     Возвращает случайный год; диапазон 1900-2021.
     :return: int
     """
-    return int(1900 + random() * 121)  # todo randint
+    return randint(1900, 2021)
 
 
 def pages() -> int:
     """
-    Возвращает случайное число страниц; диапазон 10 - 1010.
+    Возвращает случайное число страниц; диапазон 10 - 1000.
     :return: int
     """
-    return int(10 + random() * 1000)
+    return randint(10, 1000)
 
 
 def isbn() -> str:
@@ -62,7 +62,8 @@ def rating() -> float:
     Возвращает случайный рейтинг; диапазон 0-5
     :return: float
     """
-    return round(random() * 5, 2)  # uniform
+    return round(uniform(0, 5), 2)
+    # return randint(0, 500)/100 ?
 
 
 def price() -> float:
@@ -70,29 +71,31 @@ def price() -> float:
     Возвращает случайную цену; диапазон 100.00-1000.00
     :return: float
     """
-    return round(100 + random() * 1000, 2)
+    return round(uniform(100, 1000), 2)
+    # return randint(10000, 100000)/100 ?
 
 
-def authors() -> list[str]:
+def authors() -> list:
     """
     Возвращает от одного до трех случайно сгенерированных авторов
     :return: list
     """
-    return [FAKE.name() for _ in range(int(random() * 2.9) + 1)]
+    return [FAKE.name() for _ in range(randint(1, 3))]
 
 
-def book(counter: int = 1) -> Iterator[dict]:
+def book_gen(counter: int = 1) -> Iterator[dict]:
     """
     Возвращает случайно сгенерированую книгу.
     :param counter: Стартовое значение для индекса книги, по-умолчанию 1
     :return: dict
     """
+    title = titles_gen()
     while True:
         yield {
             'model': MODEL,
             'pk': counter,
             'fields': {
-                "title": next(title()),  # fixme каждый раз инициализация?
+                "title": next(title),
                 "year": year(),
                 "pages": pages(),
                 "isbn13": isbn(),
